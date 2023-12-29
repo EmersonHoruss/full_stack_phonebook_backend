d1714a79e968762dfa1990fd99adeeaf16b8465f
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2023-12-28 22:39:02 -0500

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan');
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan('tiny'));
 morgan.token('req-body', (req) => {
     if (req.method === 'POST') {
